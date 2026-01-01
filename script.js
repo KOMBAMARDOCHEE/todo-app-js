89e7e41056a50c587c80067444cc7097fd106f38
KOMBA MARDOCHEE <nkdoche@gmail.com>
2026-01-01 20:18:45 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", () => {
+  if (input.value === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  li.addEventListener("click", () => {
+    li.remove();
+  });
+
+  list.appendChild(li);
+  input.value = "";
+});
